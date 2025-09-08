d77416caa63de66a09dedb6bfe832a54c8002d2b
Jaime <114821109+JaimeWritesCode@users.noreply.github.com>
2025-09-08 22:20:54 +0800

    Update forDelivery.java

--- forDelivery.java
+++ forDelivery.java
@@ -15,12 +15,16 @@ public class forDelivery{
         System.out.println("The user has used " + x + " to order " + y);
     }
     
-    public void itemList(){
+  public void itemList(){
+         System.out.println("------------------------------------");
          clicked = null;
          System.out.println("Full ordered item list: ");
+         System.out.println("------------------------------------");
         for(int i = 0; i < cart.size(); i++){
             System.out.println(orderList.get(i));
+             System.out.println("------------------------------------");
         }
+        System.out.println("Cart status: " + clicked + " the order has been fulfilled!");
     }
     
 }
